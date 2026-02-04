12cc8a4df5db4e6e6f43e8710235ffb0cdf7f3db
yugs18 <yugs1511@gmail.com>
2026-02-04 16:51:33 +0000

    toralize - connection works

--- toralize/toralize.c
+++ toralize/toralize.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[]) {
     char *target_ip;
-    int target_port;
+    int target_port, sockfd;
+
+    struct sockaddr_in sock;
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <target_ip> <target_port>\n", argv[0]);
@@ -13,4 +15,24 @@ int main(int argc, char *argv[]) {
     }
     target_ip = argv[1];
     target_port = atoi(argv[2]);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return -1;
+    }
+
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(TORALIZE_PORT);
+    sock.sin_addr.s_addr = inet_addr(LOCALHOST_IP);
+
+    if (connect(sockfd, (struct sockaddr *)&sock, sizeof(sock))) {
+        perror("Connection to Tor failed");
+        close(sockfd);
+        return -1;
+    }
+
+    printf("[*] Connected to Tor on %s:%d\n", LOCALHOST_IP, TORALIZE_PORT);
+    close(sockfd);
+    return 0;
 }
--- toralize/toralize.h
+++ toralize/toralize.h
@@ -1,15 +1,20 @@
 /* toralize.h - Header file for toralize project */
+// https://www.openssh.org/txt/socks4.protocol
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #define LOCALHOST_IP    "127.0.0.1"
 #define TORALIZE_PORT   9050
 
-typedef unsigned char int8;
-typedef unsigned short int16;
-typedef unsigned int int32;
+// typedef unsigned char int8;
+// typedef unsigned short int16;
+// typedef unsigned int int32;
 
 /*
 
@@ -19,3 +24,32 @@ typedef unsigned int int32;
  # of bytes:	       1    1      2              4           variable       1
 
 */
+
+struct toralize_request {
+	uint8_t vn;
+	uint8_t cd;
+	uint16_t dstport;
+	uint32_t dstip;
+	unsigned char userid[8];
+};
+
+typedef struct toralize_request Req;
+
+/*
+
+				+----+----+----+----+----+----+----+----+
+				| VN | CD | DSTPORT |      DSTIP        |
+				+----+----+----+----+----+----+----+----+
+ # of bytes:	   1    1      2              4
+
+
+ */
+
+ struct toralize_response {
+	uint8_t vn;
+	uint8_t cd;
+	uint16_t unused_port;
+	uint32_t unused_ip;
+};
+
+typedef struct toralize_response Res;
